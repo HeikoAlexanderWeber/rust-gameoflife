45ddb9606dd6f71d53f9f855a66d3082c0a70d05
Heiko Alexander Weber <heiko.a.weber@gmail.com>
2019-09-28 14:17:52 +0200

    update

--- program/src/world/world.rs
+++ program/src/world/world.rs
@@ -1,3 +1,6 @@
+use std::result::Result;
+use std::ops::Range;
+
 #[derive(Debug, Clone)]
 pub struct BoundsError;
 
@@ -5,9 +8,9 @@ pub trait World {
     fn get_id(&self) -> String;
     fn get_bounds(&self) -> (usize, usize);
 
-    fn get(&self, coords: &(usize, usize)) -> std::result::Result<bool, BoundsError>;
-    fn set(&mut self, coords: &(usize, usize), alive: bool) -> std::result::Result<bool, BoundsError>;
-    fn set_span(&mut self, span: (std::ops::Range::<usize>, std::ops::Range::<usize>), alive: bool) -> std::result::Result<(), BoundsError>;
+    fn get(&self, coords: &(usize, usize)) -> Result<bool, BoundsError>;
+    fn set(&mut self, coords: &(usize, usize), alive: bool) -> Result<bool, BoundsError>;
+    fn set_span(&mut self, span: (Range::<usize>, Range::<usize>), alive: bool) -> Result<(), BoundsError>;
 
     fn get_neighbours(&self, coords: &(usize, usize)) -> u8;
 }
